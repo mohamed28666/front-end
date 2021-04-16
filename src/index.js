0c69e5bd83ee723c3b23f30599e001354470e94f
mohamed belkahla <mohamed.belkahla19@gmail.com>
2021-04-16 15:38:56 +0200

    make it better

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SignIn from './signin'
+import reportWebVitals from './reportWebVitals';
+import HideAppBar from './header';
+import DashBoard from './dashboard';
+import ThreeD from './threeD'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+
+ReactDOM.render(
+  <>
+  <HideAppBar></HideAppBar>
+  
+  <BrowserRouter>
+  <React.StrictMode>
+     <Route path="/home"><SignIn></SignIn></Route>
+     <Route path="/entered"><DashBoard></DashBoard> <ThreeD></ThreeD></Route>
+    
+  </React.StrictMode>
+  </BrowserRouter></>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/threeD.js
@@ -0,0 +1,30 @@
+import React from 'react';
+export default class ThreeD extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+       
+        
+      }
+
+
+      render() {
+         let c;
+        
+        
+       
+        
+        return (
+          <div className="threeD">
+           <iframe src="http://localhost:3333/troisd"></iframe>
+           
+          
+            
+          </div>
+        );
+
+
+
+}
+}
